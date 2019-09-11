6874db180b6fe65f06e7fa19704881cc930d5089
sophosphophyllite <son.thang.pham@gmail.com>
2019-09-10 20:45:22 -0400

    added blank componenets for the hiearchy of the project -sophos

--- MyLibrary/App.js
+++ MyLibrary/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
@@ -9,11 +9,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
