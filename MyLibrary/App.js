2b61859c95297a7dd21c8388b90bc79967f7661a
sophosphophyllite <son.thang.pham@gmail.com>
2019-09-12 22:51:55 -0400

    fixe bug to start -sophos

--- MyLibrary/App.js
+++ MyLibrary/App.js
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
